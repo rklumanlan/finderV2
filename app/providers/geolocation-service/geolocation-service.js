d41237fc848583a824607aea1da6df8627626130
rklumanlan <rklumanlan@gmail.com>
2016-05-02 16:38:59 +0800

    clean the geolocation service

--- app/providers/geolocation-service/geolocation-service.js
+++ app/providers/geolocation-service/geolocation-service.js
@@ -27,8 +27,6 @@ export class GeolocationService {
     console.log(ctr);
     var me = this;
 
-    // me.addConnectivityListeners();
-
     if(typeof google == "undefined" || typeof google.maps == "undefined"){
 
         console.log("Google maps JavaScript needs to be loaded.");
@@ -36,39 +34,12 @@ export class GeolocationService {
 
         if(me.connectivity.isOnline()){
             console.log("online, loading map");
-            console.log('1');
-
-
-
 
             let script = document.createElement("script");
             script.id = "geoLocation";
-
-            // if(me.apiKey){
-                script.src = 'https://maps.googleapis.com/maps/api/js?key='+me.apiKey +'&callback=mapInit';
-            // } else {
-            //     script.src = 'http://maps.google.com/maps/api/js?callback=mapInit';
-            // }
+            script.src = 'https://maps.googleapis.com/maps/api/js?key='+me.apiKey;
 
             document.body.appendChild(script);
-
-            //Load the SDK
-            var asd;
-            window.mapInit = function(){
-              // asd = me.getLocationName(ctr, function(result){
-              //   return result;
-              //     // $("#userLocation").text(result);
-              //     // me.geolocation =  result;
-              //     // console.log('2');
-              //     // document.getElementById('geolocation').value = me.geolocation;
-              //     // console.log(me.geolocation);
-              // });
-              // console.log(asd);
-            }
-            console.log(window.mapInit);
-
-            console.log(me.geolocation);
-            console.log('3');
 
         }
         else {
@@ -90,87 +61,14 @@ export class GeolocationService {
     }
   }
 
-  addConnectivityListeners(){
-    var me = this;
-
-    var onOnline = function(){
-        setTimeout(function(){
-            if(typeof google == "undefined" || typeof google.maps == "undefined"){
-                me.loadGoogleMaps();
-            } else {
-                if(!me.mapInitialised){
-                    me.initMap(ctr);
-                }
-
-                // me.enableMap();
-            }
-        }, 2000);
-    };
-
-    var onOffline = function(){
-        // me.disableMap();
-    };
-
-    document.addEventListener('online', onOnline, false);
-    document.addEventListener('offline', onOffline, false);
-
-  }
-
-  initMap(ctr){
-    var me = this;
-    me.mapInitialised = true;
-    // return ctr;
-    // var geocoder = new google.maps.Geocoder();
-    // var infowindow = new google.maps.InfoWindow();
-    // var input = ctr;
-    // var latlngStr = input.split(',', 2);
-    // // var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
-    // // var bb;
-    // var locationName;
-    // var geocoder = new google.maps.Geocoder();
-    // var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
-    //
-    // // Reverse Geocoding using google maps api.
-    // geocoder.geocode({ 'latLng': latlng }, function (results, status) {
-    //     if (status == google.maps.GeocoderStatus.OK) {
-    //         if (results[1]) {
-    //             locationName = results[1].formatted_address;
-    //             alert(locationName);
-    //         }
-    //         else {
-    //             locationName = "Unknown";
-    //         }
-    //     }
-    //     else {
-    //         locationName = "Couldn't find location. Error code: " + status;
-    //     }
-    //     alert(locationName);
-    //     callback(locationName);
-    // });
-    // console.log(locationName);
-  }
   setLocationName(ctr){
     var me = this;
     var geo;
     me.getLocationName(ctr, function(result){
-      // return result;
       console.log(result);
-        // $("#userLocation").text(result);
         geo =  result;
-        // console.log('2');
-        // document.getElementById('geolocation').value = me.geolocation;
-        // console.log(me.geolocation);
     });
 
-    // setTimeout(function() {
-    //   me.geolocation = geo;
-    //   console.log('time');
-    // }, 5000);
-    //
-    //
-    //
-    // console.log(me.geolocation);
-    // return me.geolocation;
     return new Promise(function(resolve, reject) {
       // Only `delay` is able to resolve or reject the promise
       setTimeout(function() {
@@ -202,8 +100,6 @@ export class GeolocationService {
           else {
               locationName = "Couldn't find location. Error code: " + status;
           }
-          alert(locationName);
-
           // document.getElementById('geolocation').value = locationName;
           callback(locationName);
       });
